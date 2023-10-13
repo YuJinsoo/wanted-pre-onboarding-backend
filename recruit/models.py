de2b23daf81308c6ef16f4bb89dd542eb6682e59
YuJinsoo <realaqua23@gmail.com>
2023-10-13 22:45:58 +0900

    [개발]database 모델링 JobOpening, Company

--- recruit/models.py
+++ recruit/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class JobOpening(models.Model):
+    company = models.ForeignKey('Company', on_delete=models.CASCADE)
+    position = models.CharField(max_length=100)
+    content = models.TextField(max_length=500)
+    tech = models.CharField(max_length=100)
+    reward = models.PositiveIntegerField(default=0)
+    
+    def __str__(self):
+        return f"JobOpening[{self.pk}]: {self.company}"
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return f"<Company[{self.id}]:{self.name}>"
